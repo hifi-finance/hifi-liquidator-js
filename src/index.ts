cdd6e0460047cf946e0acad14bbad20761d33fdb
scorpion9979 <ahmed.i.tawfeeq@protonmail.com>
2021-09-03 03:56:40 +0300

    feat: added basic bot layout

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-const foo: string = "Hello World";
-console.log(foo);
+import * as networks from "./networks.json";
+
+import { Wallet, providers, utils } from "ethers";
+
+import { Bot } from "./bot/index";
+import { NetworkName } from "./types";
+import { isTrueSet } from "./helpers";
+
+require("dotenv").config();
+
+const {
+  ALCHEMY_KEY,
+  PERSISTENCE = "true",
+  SELECTED_ACCOUNT = "0",
+  SILENT_MODE,
+  WALLET_SEED,
+} = process.env as { [key: string]: string };
+
+const { NETWORK_NAME = "matic" } = process.env as { NETWORK_NAME: NetworkName };
+
+const account = utils.HDNode.fromMnemonic(WALLET_SEED as string).derivePath(`m/44'/60'/0'/0/${SELECTED_ACCOUNT}`);
+
+const provider = new providers.AlchemyProvider(NETWORK_NAME, ALCHEMY_KEY);
+
+const signer = new Wallet(account, provider);
+
+new Bot({
+  network: networks[NETWORK_NAME],
+  persistence: isTrueSet(PERSISTENCE),
+  provider,
+  signer,
+  silentMode: isTrueSet(SILENT_MODE),
+}).run();
