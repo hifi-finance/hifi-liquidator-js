adcabd19513233a2e22de504468745691bdb76f5
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2024-01-24 15:11:07 -0400

    chore: add mainnet forking to hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,16 @@
 import "@nomiclabs/hardhat-ethers";
 import type { HardhatUserConfig } from "hardhat/config";
+import { config as dotenvConfig } from "dotenv";
+import { resolve } from "path";
+import networkConfig from "./src/network-config.json";
+
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
+dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
+
+const alchemyKey: string | undefined = process.env.ALCHEMY_KEY;
+if (!alchemyKey) {
+  throw new Error("Please set your ALCHEMY_KEY in a .env file");
+}
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -9,6 +20,10 @@ const config: HardhatUserConfig = {
         mnemonic: "test test test test test test test test test test test junk",
       },
       chainId: 31337,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyKey}`,
+        blockNumber: networkConfig.homestead.startBlock,
+      },
     },
   },
   paths: {
