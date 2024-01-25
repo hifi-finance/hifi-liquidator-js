c0398884b7ea385046c91eef8d01878d15a03fe3
Ahmed Ihsan Tawfeeq <ahmed.i.tawfeeq@protonmail.com>
2024-01-25 19:33:05 -0400

    test: add "HTOKENS" to test constants

--- test/shared/constants.ts
+++ test/shared/constants.ts
@@ -2,3 +2,4 @@ import networkConfig from "../../src/network-config.json";
 
 export const BALANCESHEET_ADDRESS: string = networkConfig.homestead.contracts.balanceSheet;
 export const FORK_BLOCK: number = networkConfig.homestead.startBlock;
+export const HTOKENS: string[] = ["0x4EC7101B179c28e4332ED5B06174b38AeE18cf32"];
